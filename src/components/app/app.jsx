0793572e2c34a7ece02a95ad06c23ed8e555210b
Roma <romankechko98@yandex.ru>
2023-09-18 13:44:35 +0600

    sprint 11. коммит 3

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import style from "./App.module.css";
+import style from "./app.module.css";
 import AppHeader from "../App-Header/App-Header";
 import BurgerIngredients from "../Burger-Ingredients/Burger-Ingredients";
 import BurgerConstructor from "../Burger-Constructor/Burger-Constructor";
