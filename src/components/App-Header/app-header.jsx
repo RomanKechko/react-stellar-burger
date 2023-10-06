de2d56ba323fb50fc505187fac3e892efeef075c
Roma <romankechko98@yandex.ru>
2023-10-06 12:38:25 +0600

    вернул файлы. для кеша

---
+++ src/components/App-Header/app-header.jsx
@@ -0,0 +1,36 @@
+import {
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import React from "react";
+import styles from "./app-header.module.css";
+import cn from "classnames";
+
+function AppHeader() {
+  return (
+    <header className={cn(styles.header, "pb-4 pt-4")}>
+      <nav className={styles.nav}>
+        <div className={styles.link__column_left}>
+          <a href="#" className={cn(styles.link, "pb-4 pt-4 pl-5 pr-5")}>
+            <BurgerIcon type="primary" />
+            <p className="text text_type_main-default">Конструктор</p>
+          </a>
+          <a href="#" className={cn(styles.link, "pb-4 pt-4 pl-5 pr-5")}>
+            <ListIcon type="primary" />
+            <p className="text text_type_main-default">Лента заказов</p>
+          </a>
+        </div>
+        <Logo />
+        <div className={styles.link__column_right}>
+          <a href="#" className={cn(styles.link, "pb-4 pt-4 pl-5 pr-5")}>
+            <ProfileIcon type="primary" />
+            <p className="text text_type_main-default">Личный кабинет</p>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+export default AppHeader;
---
+++ src/components/Burger-Ingredients/burger-ingredients.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useMemo, useRef } from "react";
+import style from "./burger-ingredients.module.css";
+import cn from "classnames";
+import Tabs from "../tabs/tabs";
+import BurgerIngredientCategory from "../burger-ingredient-category/burger-ingredient-category";
+import IngredientDetails from "../ingredient-details/ingredient-details";
+import Modal from "../modal/modal";
+import { useSelector } from "react-redux";
+import { useInView } from "react-intersection-observer";
+
+function BurgerIngredients() {
+  const [currentTab, setCurrentTab] = React.useState("buns");
+
+  const list =
+    useSelector((state) => state.ingredientsReducer.dataIngridients?.data) ||
+    [];
+
+  const buns = useMemo(
+    () => list.filter((item) => item.type === "bun"),
+    [list]
+  );
+  const mains = useMemo(
+    () => list.filter((item) => item.type === "main"),
+    [list]
+  );
+  const sauces = useMemo(
+    () => list.filter((item) => item.type === "sauce"),
+    [list]
+  );
+
+  const ingredient = useSelector(
+    (state) => state.modalIngridientReducer.ingredient
+  );
+
+  const onClickTab = (tab) => {
+    setCurrentTab(tab);
+    const element = document.getElementById(tab);
+    if (element) element.scrollIntoView({ behavior: "smooth" });
+  };
+
+  /* console.log(bun); */
+
+  const [bunsRef, inViewBuns] = useInView({
+    threshold: 0,
+  });
+  const [mainsRef, inViewFilling] = useInView({
+    threshold: 0,
+  });
+  const [saucesRef, inViewSauces] = useInView({
+    threshold: 0,
+  });
+  console.log(currentTab);
+  useEffect(() => {
+    if (inViewBuns) {
+      setCurrentTab("buns");
+    } else if (inViewSauces) {
+      setCurrentTab("sauces");
+    } else if (inViewFilling) {
+      setCurrentTab("mains");
+    }
+  }, [inViewBuns, inViewFilling, inViewSauces]);
+
+  return (
+    <section className={cn(style.section, "pt-10")}>
+      <h1 className={cn(style.title, "text text_type_main-large")}>
+        Соберите бургер
+      </h1>
+      <Tabs currentTab={currentTab} onClickTab={onClickTab} />
+      <div className={cn(style.container, "pt-10")}>
+        <BurgerIngredientCategory
+          title="Булки"
+          titleId="buns"
+          data={buns}
+          refs={bunsRef}
+        />
+        <BurgerIngredientCategory
+          title="Начинка"
+          titleId="mains"
+          data={mains}
+          refs={mainsRef}
+        />
+        <BurgerIngredientCategory
+          title="Соусы"
+          titleId="sauces"
+          data={sauces}
+          refs={saucesRef}
+        />
+      </div>
+      {ingredient && (
+        <Modal>
+          <IngredientDetails ingredient={ingredient} />
+        </Modal>
+      )}
+    </section>
+  );
+}
+
+export default BurgerIngredients;
